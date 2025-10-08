d83a8d50f85d83bfff3185e5eb4ac789315c4e8e
Jaimlja <andreyev.vv@gmail.com>
2025-10-08 20:22:57 +0300

    commit 08.10.2025 locally work

--- server_codespace/websocket-for-codespaces/models/connections.js
+++ server_codespace/websocket-for-codespaces/models/connections.js
@@ -41,8 +41,8 @@ const docsConnection = makeNewConnection('mongodb+srv://Jaimlja:%5FGirolamo%5FCa
 */
 
 //DBs in docker containers, server on PC
-const authConnection = makeNewConnection('mongodb://user:pas----sword@localhost:27017/mongo_auth?authSource=admin');
-const docsConnection = makeNewConnection('mongodb://user:pas----sword@localhost:27017/mongo_documents?authSource=admin');
+const authConnection = makeNewConnection('mongodb://user:password@mongo:27017/mongo_auth?authSource=admin&replicaSet=rs0');
+const docsConnection = makeNewConnection('mongodb://user:password@mongo:27017/mongo_documents?authSource=admin&replicaSet=rs0');
 
 //DBs and server in docker containers
 /*
